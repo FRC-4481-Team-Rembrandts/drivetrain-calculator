fb1425e3ea5d892b792b40c7b1380a7bc7660163
JvdBroekTue <j.v.d.broek@student.tue.nl>
2023-07-13 16:53:11 +0200

    Added a_x vs T_stall graph

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const layout = {title: "Beautiful graph",
-                xaxis: {range: [0, 1]},
-                yaxis: {range: [0, 10]}};
+const layoutH = {title: "Maximum acceleration against center of gravity height",
+                xaxis: {range: [0, 1],
+                        title: "Height of CG [m]"},
+                yaxis: {range: [0, 10],
+                        title: "Maximum acceleration [m/s^2]"}};
 
+const layoutT = {title: "Maximum acceleration against motor stall torque",
+                xaxis: {range: [0, 4],
+                        title: "Motor stall torque [Nm]"},
+                yaxis: {range: [0, 10],
+                        title: "Maximum acceleration [m/s^2]"}};
 
 function inputChange(){
     let mu = parseFloat(document.getElementById('number_input_mu').value);
     let l = parseFloat(document.getElementById('number_input_l').value);
     let a = parseFloat(document.getElementById('number_input_a').value);
+    let h = parseFloat(document.getElementById('number_input_h').value);
     let m = parseFloat(document.getElementById('number_input_m').value);
     let r_t = parseFloat(document.getElementById('number_input_r_t').value);
     let T_stall = parseFloat(document.getElementById('number_input_T_stall').value);
     let gear_ratio = parseFloat(document.getElementById('number_input_gear_ratio').value);
 
-    [xValues,yValues] = formula(mu, l, a, m, r_t, T_stall, gear_ratio);
-    const data = [{x:xValues, y:yValues, mode:"lines"}];
+    [xValuesH,yValuesH] = formula_ax_vs_h(mu, l, a, m, r_t, T_stall, gear_ratio);
+    const dataH = [{x:xValuesH, y:yValuesH, mode:"lines"}];
 
-    Plotly.newPlot("plotWindow", data, layout);
+    Plotly.newPlot("plot_window_ax_vs_h", dataH, layoutH);
+
+    [xValuesTStall,yValuesTStall] = formula_ax_vs_t_stall(mu, l, a, h, m, r_t, gear_ratio);
+    const dataT = [{x:xValuesTStall, y:yValuesTStall, mode:"lines"}];
+
+    Plotly.newPlot("plot_window_ax_vs_t_stall", dataT, layoutT);
 }
