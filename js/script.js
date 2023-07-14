af718c4da905afea6287168572167930ebbf28c5
JvdBroekTue <j.v.d.broek@student.tue.nl>
2023-07-14 17:40:31 +0200

    Optimized javascript formula functions

--- js/script.js
+++ js/script.js
@@ -13,19 +13,18 @@ const layoutT = {title: "Maximum acceleration against motor stall torque",
 function inputChange(){
     let mu = parseFloat(document.getElementById('number_input_mu').value);
     let l = parseFloat(document.getElementById('number_input_l').value);
-    let a = parseFloat(document.getElementById('number_input_a').value);
     let h = parseFloat(document.getElementById('number_input_h').value);
     let m = parseFloat(document.getElementById('number_input_m').value);
     let r_t = parseFloat(document.getElementById('number_input_r_t').value);
     let T_stall = parseFloat(document.getElementById('number_input_T_stall').value);
     let gear_ratio = parseFloat(document.getElementById('number_input_gear_ratio').value);
 
-    [xValuesH,yValuesH] = formula_ax_vs_h(mu, l, a, m, r_t, T_stall, gear_ratio);
+    [xValuesH,yValuesH] = formula_ax_vs_h(mu, l, m, r_t, T_stall, gear_ratio);
     const dataH = [{x:xValuesH, y:yValuesH, mode:"lines"}];
 
     Plotly.newPlot("plot_window_ax_vs_h", dataH, layoutH);
 
-    [xValuesTStall,yValuesTStall] = formula_ax_vs_t_stall(mu, l, a, h, m, r_t, gear_ratio);
+    [xValuesTStall,yValuesTStall] = formula_ax_vs_t_stall(mu, l, h, m, r_t, gear_ratio);
     const dataT = [{x:xValuesTStall, y:yValuesTStall, mode:"lines"}];
 
     Plotly.newPlot("plot_window_ax_vs_t_stall", dataT, layoutT);
